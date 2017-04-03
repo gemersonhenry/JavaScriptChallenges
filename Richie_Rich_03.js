adc79512ac7249f71c1ab294a5408f950117f39a
gemersonhenry <henryingtel@gmail.com>
2017-04-03 05:44:48 +0100

    reto numero canonico - tercera forma

---
+++ Richie_Rich_03.js
@@ -0,0 +1,10 @@
+function main() {
+    //var n_temp = readLine().split(' ');
+    var n = parseInt('6');
+    var k = parseInt('2');
+    var number = '932239';
+
+    var NumeroCanonico = function () {
+    	
+    }
+}
